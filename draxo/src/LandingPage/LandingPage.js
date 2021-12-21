e36e3b8eed3f74a20c4371ec528d197c78e237a2
jdcera4 <jdcera4@gmail.com>
2021-12-20 21:52:50 -0500

    component principal

---
+++ draxo/src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+import { Footer } from '../footer/index'
+
+function LandingPage() {
+  return (
+    <React.Fragment>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export {LandingPage};
---
+++ draxo/src/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LandingPage } from './LandingPage';
+
+
+function App() {
+  return (
+    <React.StrictMode>
+      <LandingPage />
+    </React.StrictMode>
+  );
+}
+
+export default App;
